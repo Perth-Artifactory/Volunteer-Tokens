b1d801f2e7d24f30cd57e95937a6c6962835d562
Fletcher <me@fboyd.me>
2025-09-27 08:55:44 +0800

    Fix typo in explainer

--- editable_resources/strings.py
+++ editable_resources/strings.py
@@ -6,8 +6,7 @@ with open("config.json") as f:
 
 unrecognised = """Unfortunately I don't recognise you. This system is only available to members, if you think this is a mistake please contact #it."""
 header = "Welcome to the volunteer token system!"
-explainer = "This system allows you to track the time you've volunteered for the organisation and your progress towards specific rewards. Your contribution towards making the Artifactory the place it is is greatly appreciated!"
-hours_summary = "Last month you volunteered for *{last_month}* hours. You have volunteered a total of *{total_hours}* hours since the system was implemented.\nSo far this month we've processed *{this_month}* hours of tokens for you. Remember, tokens are processed manually and make take a while to show up!"
+explainer = "This system allows you to track the time you've volunteered for the organisation and your progress towards specific rewards. Your contribution towards making the Artifactory the place it is and is greatly appreciated!"
 stats_explainer = "Every hour you contribute to our community is valuable and appreciated. Whether it's a one-off event or a regular commitment, your time helps us thrive. Here are some statistics about your volunteering journey so far."
 no_active_rewards = "Unfortunately there are no active reward tiers recorded for you last month. (Remember: physical tokens are processed manually so may take a few days to appear here)"
 admin_explainer = "As an admin you have some extra tools available to you below. Please use them responsibly."
